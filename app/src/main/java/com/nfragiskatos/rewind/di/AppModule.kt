84635e072063de3aeb38f13b1966e9ae4bd929f6
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-05-01 16:35:53 -0700

    - created DTO for movie objects received from the API - created DTO for a paged result of movie objects from the API - created hilt module for providing singletons - just the retrofit object built with the TheMovieDbApi interface for now

---
+++ app/src/main/java/com/nfragiskatos/rewind/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.nfragiskatos.rewind.di
+
+import com.nfragiskatos.rewind.BuildConfig
+import com.nfragiskatos.rewind.data.remote.TheMovieDbApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesTheMoveDbApi(): TheMovieDbApi {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.THE_MOVIE_DB_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
+    }
+}
