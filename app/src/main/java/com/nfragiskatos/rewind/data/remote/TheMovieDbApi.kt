ab433c2120d2caba3e6e94e4c2c75562616d6627
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-05-02 19:55:34 -0700

    - changed return types to Response<>

--- app/src/main/java/com/nfragiskatos/rewind/data/remote/TheMovieDbApi.kt
+++ app/src/main/java/com/nfragiskatos/rewind/data/remote/TheMovieDbApi.kt
@@ -3,6 +3,7 @@ package com.nfragiskatos.rewind.data.remote
 import com.nfragiskatos.rewind.BuildConfig
 import com.nfragiskatos.rewind.data.remote.dto.MoviePagedResultsDto
 import com.nfragiskatos.rewind.data.remote.dto.TvShowPagedResultsDto
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,18 +12,18 @@ interface TheMovieDbApi {
     @GET("trending/movie/week")
     suspend fun getTrendingMoviesForWeek(
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DB_API_KEY
-    ): MoviePagedResultsDto
+    ): Response<MoviePagedResultsDto>
 
     @GET("search/tv")
     suspend fun searchTvShowsByQuery(
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DB_API_KEY,
         @Query("query") query: String
-    ): TvShowPagedResultsDto
+    ): Response<TvShowPagedResultsDto>
 
     @GET("search/movie")
     suspend fun searchMoviesByQuery(
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DB_API_KEY,
         @Query("query") query: String
-    ): MoviePagedResultsDto
+    ): Response<MoviePagedResultsDto>
 
 }
